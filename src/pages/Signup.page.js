84c3f00786665fb03187cd2ddb15d581a8334c13
jackson <jacksondesobral@hotmail.com>
2024-07-03 10:54:57 -0300

    correções de idioma e layout

--- src/pages/Signup.page.js
+++ src/pages/Signup.page.js
@@ -1,47 +1,63 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import { Card } from '@mui/material';
+
 import { Button, TextField } from "@mui/material";
 import { useContext, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { UserContext } from "../pages/contexts/user.context";
- 
-const Signup = () => {
- const navigate = useNavigate();
- const location = useLocation();
- 
- // As explained in the Login page.
- const { emailPasswordSignup } = useContext(UserContext);
- const [form, setForm] = useState({
-   email: "",
-   password: ""
- });
- 
- // As explained in the Login page.
- const onFormInputChange = (event) => {
-   const { name, value } = event.target;
-   setForm({ ...form, [name]: value });
- };
- 
- 
- // As explained in the Login page.
- const redirectNow = () => {
-   const redirectTo = location.search.replace("?redirectTo=", "");
-   navigate(redirectTo ? redirectTo : "/");
- }
- 
- // As explained in the Login page.
- const onSubmit = async () => {
-   try {
-     const user = await emailPasswordSignup(form.email, form.password);
-     if (user) {
-       redirectNow();
-     }
-   } catch (error) {
-     alert(error);
-   }
- };
- 
- return <form style={{ display: "flex", flexDirection: "column", maxWidth: "300px", margin: "auto" }}>
-   <h1>Signup</h1>
-   <TextField
+
+import styles from './Signup.page.module.css'
+
+  const Signup = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    
+    // As explained in the Login page.
+    const { emailPasswordSignup } = useContext(UserContext);
+    const [form, setForm] = useState({
+      email: "",
+      password: ""
+    });
+    
+    // As explained in the Login page.
+    const onFormInputChange = (event) => {
+      const { name, value } = event.target;
+      setForm({ ...form, [name]: value });
+    };
+    
+    
+    // As explained in the Login page.
+    const redirectNow = () => {
+      const redirectTo = location.search.replace("?redirectTo=", "");
+      navigate(redirectTo ? redirectTo : "/");
+    }
+    
+    // As explained in the Login page.
+    const onSubmit = async () => {
+      try {
+        const user = await emailPasswordSignup(form.email, form.password);
+        if (user) {
+          redirectNow();
+        }
+      } catch (error) {
+        alert(error);
+      }
+    };
+
+  return (
+    <Container maxWidth="sm" >
+    
+    <Card sx={{ 
+      bgcolor: 'white', 
+      height: '70vh',
+      borderRadius: '5%',
+      marginTop: '10vh',
+      }}
+    >
+      <form style={{ display: "flex", flexDirection: "column", maxWidth: "300px", margin: "auto", marginTop: '7vh' }}>
+        <h1 className={styles.title}>Cadastre-se</h1>
+        <TextField
      label="Email"
      type="email"
      variant="outlined"
@@ -50,20 +66,26 @@ const Signup = () => {
      onInput={onFormInputChange}
      style={{ marginBottom: "1rem" }}
    />
-   <TextField
-     label="Password"
-     type="password"
+         <TextField
+     label="Senha"
+     type="Senha"
      variant="outlined"
-     name="password"
+     name="Senha"
      value={form.password}
      onInput={onFormInputChange}
      style={{ marginBottom: "1rem" }}
    />
-   <Button variant="contained" color="primary" onClick={onSubmit}>
-     Signup
+
+<Button variant="contained" color="primary" onClick={onSubmit}>
+     Enviar Cadastro
    </Button>
-   <p>Have an account already? <Link to="/login">Login</Link></p>
- </form>
+        <p>Já tem conta? <Link to="/login">Login</Link></p>
+      </form>
+    </Card>
+   
+
+    </Container>
+  );
 }
- 
+
 export default Signup;
