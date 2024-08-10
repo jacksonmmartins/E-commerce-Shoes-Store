3ea10db68e2626914318afd046fc72189c3a25fa
jackson <jacksondesobral@hotmail.com>
2024-08-10 16:42:49 -0300

    correção_layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import BookCard from '../pages/components/BookCard';
+
+
 function Home(){
+    const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://mern-omega-ten.vercel.app/api/books')
+      .then((res) => {
+        setBooks(res.data);
+      })
+      .catch((err) => {
+        console.log('Error from ShowBookList');
+      });
+  }, []);
+
+  const bookList =
+    books.length === 0
+      ? 'there is no book record!'
+      : books.map((book, k) => <BookCard book={book} key={k} />);
+
     return(
-        <h1>
-            Página incial
-        </h1>
+        <div className='list'>{bookList}</div>
     )
 }
 
