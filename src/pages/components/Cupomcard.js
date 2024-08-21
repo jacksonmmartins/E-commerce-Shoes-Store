699ff5bbfa88a600950f182c7db369bbc095ceb3
Jackson <jacksondesobral@hotmail.com>
2024-08-21 20:57:30 -0300

    cards

--- src/pages/components/Cupomcard.js
+++ src/pages/components/Cupomcard.js
@@ -1,27 +1,34 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Desconto from './Img/desconto.png'
 
-const BookCard = ({book}) => {
+const BookCard = ({ book }) => {
 
   return (
-    <div className='card-container'>
-       <Link to={`/show-book/${book._id}`} class='disable_links'>
-      <img
-        src='https://img.freepik.com/fotos-gratis/frente-do-
-        icone-de-porcentagem_187299-40103.jpg?t=st=1723604193~exp=1723607793~
-        hmac=e47a4eabe5782b4f97921a2bcb9625fdbfc531448370c447153598a1c0911ccf&w
-        =740'
-        alt='Books'
-        height={200}
-      />
-      <div className='desc'>
-        <h2>
-         {book.title}
-        </h2>
-        <h3>{book.author}</h3>
-        <p>{book.description}</p>
-      </div>
-      </Link>
+    <div className='card-container text-center'>
+      <Card>
+        <br />
+        <Link to={`/show-book/${book._id}`} class='disable_links'>
+          <img
+            src={Desconto}
+            alt='Books'
+            height={200}
+            width={200}
+          />
+          <div className='desc'>
+            <br />
+            <h2>
+              {book.title}
+            </h2>
+            <h3>{book.author}</h3>
+            <p>{book.description}</p>
+          </div>
+        </Link>
+        <br />
+        <br />
+        <br />
+      </Card>
     </div>
   );
 };
