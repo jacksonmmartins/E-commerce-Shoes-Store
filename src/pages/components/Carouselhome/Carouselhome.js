8f18b19afafa6224e60891a41b29aac84c396b0c
Jackson <jacksondesobral@hotmail.com>
2025-08-08 11:48:16 -0300

    otimização

--- src/pages/components/Carouselhome/Carouselhome.js
+++ src/pages/components/Carouselhome/Carouselhome.js
@@ -1,40 +1,38 @@
 import React from 'react';
-import { MDBCarousel, MDBCarouselItem, MDBCarouselCaption } from 'mdb-react-ui-kit';
-
-//Imports dos destaques
-//import banner from '../Img/Banner.png'
-import Promo from '../Img/Ofertas do Dia.png';
-import Novidades from '../Img/Novidades.png';
-import Cadastro from '../Img/Baner_cadastro.png'
-import { Card } from 'react-bootstrap';
+import { MDBCarousel, MDBCarouselItem } from 'mdb-react-ui-kit';
+import Promo from '../Img/Ofertas do Dia.webp';
+import Novidades from '../Img/Novidades.webp';
+import Cadastro from '../Img/Baner_cadastro.webp';
 
 export default function Carouselhome() {
   return (
-    <>
-    <MDBCarousel showControls fade >
-      <MDBCarouselItem >
-        <Card>
-        <img src={Promo} className='d-block w-100 round' alt='...' />
-        </Card>
-        <MDBCarouselCaption>
-
-        </MDBCarouselCaption>
+    <MDBCarousel showControls fade>
+      <MDBCarouselItem>
+        <img
+          src={Promo}
+          className='d-block w-100 round'
+          alt='Ofertas do Dia'
+          loading='eager' // imagem principal
+        />
       </MDBCarouselItem>
 
-      <MDBCarouselItem >
-        <img src={Novidades} className='d-block w-100 round' alt='...' />
-        <MDBCarouselCaption>
-
-        </MDBCarouselCaption>
+      <MDBCarouselItem>
+        <img
+          src={Novidades}
+          className='d-block w-100 round'
+          alt='Novidades'
+          loading='lazy'
+        />
       </MDBCarouselItem>
 
-      <MDBCarouselItem >
-        <img src={Cadastro} className='d-block w-100 round' alt='...' />
-        <MDBCarouselCaption>
-
-        </MDBCarouselCaption>
+      <MDBCarouselItem>
+        <img
+          src={Cadastro}
+          className='d-block w-100 round'
+          alt='Cadastro'
+          loading='lazy'
+        />
       </MDBCarouselItem>
     </MDBCarousel>
-    </>
   );
 }
